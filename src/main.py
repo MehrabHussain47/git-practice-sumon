5f9e352a1680f1a245b479254ebec309916c8091
Asus <mehhussumon47@gmail.com>
2026-06-12 18:37:49 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 from datetime import date
+from utils import add, subtract
+
+a = 10
+b = 5
 
 print("Md. Mehrab Hussain Sumon")
 print("Today's Date:", date.today())
+print("Addition:", add(a, b))
+print("Subtration:", subtract(a, b))
