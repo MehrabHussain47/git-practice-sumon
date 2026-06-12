1ecf55f6a7a7d20a12f7d01eb102cd43b00c4275
Asus <mehhussumon47@gmail.com>
2026-06-12 18:45:03 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 a = 10
 b = 5
@@ -8,3 +8,4 @@ print("Md. Mehrab Hussain Sumon")
 print("Today's Date:", date.today())
 print("Addition:", add(a, b))
 print("Subtration:", subtract(a, b))
+print("Multiplication:", multiply(a, b))
